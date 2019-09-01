055acb7a7686aeb6f35b1f79da65acfed8589f49
daniellebs <daniellebs@users.noreply.github.com>
2019-09-01 17:43:25 +0300

    Create Neo4j client and basic graph generator
    This change adds a new Neo4j client that allows adding nodes and edges to the graph.
    
    In addition, a new grid based graph generateor was created. More validations are required, as well as edge weights.

---
+++ data_processing/neo4j_client.py
@@ -0,0 +1,92 @@
+import argparse
+import json
+import logging
+from neo4j import GraphDatabase, basic_auth
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(
+    description='Constructs and handles Neo4j graph based on given '
+                'nodes and links.')
+parser.add_argument('--credentials', metavar='C', nargs=1, type=str,
+                    help='Path to credentials JSON file')
+
+args = parser.parse_args()
+
+
+class UnimplementedException(BaseException):
+    pass
+
+
+class Neo4jClient(object):
+    def __init__(self, uri, user, password, log_level=logging.INFO):
+        # logging.basicConfig(filename="neo4j.log",
+        #                     format='%(asctime)s %(message)s',
+        #                     filemode='w')
+        self._logger = logging.getLogger()
+        self._logger.setLevel(log_level)
+        self._logger.info(f'Connecting to {uri}')
+        self._driver = GraphDatabase.driver(uri,
+                                            auth=basic_auth(user, password))
+
+    def close(self):
+        self._driver.close()
+
+    def add_nodes(self, nodes_df, *argv):
+        """Adds given nodes to the connected graph.
+
+        :param nodes_df: pandas dataframe containing list of nodes and their
+                         properties.
+        :param argv: arguments representing the nodes' properties names.
+        :return: None
+        """
+        with self._driver.session() as session:
+            for index, row in tqdm(nodes_df.iterrows()):
+                arguments = {}
+                for arg in argv:
+                    arguments[arg] = row[arg]
+                session.write_transaction(self._add_node, **arguments)
+
+    @staticmethod
+    def _add_node(tx, **kwargs):
+        """Adds a single node to the graph using the provided transaction.
+
+        Must be overridden.
+        :param tx: transaction to run.
+        :param kwargs: keyword parameters for the node properties.
+        :return: None
+        """
+        raise UnimplementedException()
+
+    def add_edges(self, nodes_df, *argv):
+        """Adds given edges to the connected graph.
+
+        :param nodes_df: pandas dataframe containing list of edges.
+        :param argv: arguments representing the nodes' properties names.
+        :return: None
+        """
+        with self._driver.session() as session:
+            for index, row in tqdm(nodes_df.iterrows()):
+                arguments = {}
+                for arg in argv:
+                    arguments[arg] = row[arg]
+                session.write_transaction(self._add_edge, **arguments)
+
+    @staticmethod
+    def _add_edge(tx, **kwargs):
+        """Adds a single edge to the graph using the provided transaction.
+
+        Must be overridden.
+        :param tx: transaction to run.
+        :param kwargs: keyword parameters for the node properties to match.
+        :return: None
+        """
+        raise UnimplementedException()
+
+
+if __name__ == '__main__':
+    # Verify the connection succeeds
+    with open(args.credentials[0]) as f:
+        credentials = json.load(f)
+    h = Neo4jClient(credentials['neo4j']['uri'], credentials['neo4j']['user'],
+                    credentials['neo4j']['password'])
+    h.close()
---
+++ data_processing/testing/grid_graph_generator.py
@@ -0,0 +1,105 @@
+from contextlib import redirect_stderr
+
+from data_processing.neo4j_client import Neo4jClient, args
+import json
+import logging
+import numpy as np
+import pandas as pd
+
+
+class GraphGenerator(Neo4jClient):
+    def __init__(self, uri, user, password, number_of_rows, number_of_columns,
+                 edge_probability=0.5, weight_mean=1, weight_std=0.2,
+                 dry_run=False):
+        super(GraphGenerator, self).__init__(uri, user, password)
+        self._number_of_rows = number_of_rows
+        self._number_of_cols = number_of_columns
+        self._edge_prob = edge_probability
+        self._weight_mean = weight_mean
+        self._weight_std = weight_std
+        self._nodes = None
+        self._edges = []
+        self._dry_run = dry_run
+
+    def bernoulli_trial(self):
+        return np.random.random() > self._edge_prob
+
+    def weight(self):
+        return np.random.normal(self._weight_mean, self._weight_std)
+
+    def generate_planar_grid_graph(self):
+        # Create cartesian product
+        rows = range(self._number_of_rows)
+        cols = range(self._number_of_cols)
+        self._nodes = [(r, c) for r in rows for c in cols]
+        self._logger.info(f'Generated {len(self._nodes)} nodes.')
+        for i, j in self._nodes:
+            # Can also be done by iterating from (i-1,j-1) to (i+1,j+1).
+            if i > 0 and j - 1 and self.bernoulli_trial():
+                self._edges.append((i, j, i - 1, j - 1, self.weight()))
+            if i > 0 and self.bernoulli_trial():
+                self._edges.append((i, j, i - 1, j, self.weight()))
+            if i > 0 and j < self._number_of_cols - 1 \
+                    and self.bernoulli_trial():
+                self._edges.append((i, j, i - 1, j + 1, self.weight()))
+            if j < self._number_of_cols - 1 and self.bernoulli_trial():
+                self._edges.append((i, j, i, j + 1, self.weight()))
+            if i < self._number_of_rows - 1 and j < self._number_of_cols - 1 \
+                    and self.bernoulli_trial():
+                self._edges.append((i, j, i + 1, j + 1, self.weight()))
+            if i < self._number_of_rows - 1 and self.bernoulli_trial():
+                self._edges.append((i, j, i + 1, j, self.weight()))
+            if i < self._number_of_rows - 1 and j > 0 \
+                    and self.bernoulli_trial():
+                self._edges.append((i, j, i + 1, j - 1, self.weight()))
+            if j > 0:
+                self._edges.append((i, j, i, j - 1, self.weight()))
+
+        self._logger.info(f'Generated {len(self._edges)} edges.')
+
+        # Add nodes to graph
+        nodes_columns = ['i', 'j']
+        self.add_nodes(pd.DataFrame(self._nodes, columns=nodes_columns),
+                       *nodes_columns)
+
+        # Add edges to graph
+        edges_columns = ['from_i', 'from_j', 'to_i', 'to_j', 'w']
+        self.add_edges(pd.DataFrame(self._edges, columns=edges_columns),
+                       *edges_columns)
+
+    def get_number_of_nodes(self):
+        return len(self._nodes)
+
+    def get_number_of_edges(self):
+        return len(self._edges)
+
+    @staticmethod
+    def _add_node(tx, **kwargs):
+        """Override"""
+        tx.run("CREATE (a:Node) "
+               "SET a.i = $i "
+               "SET a.j = $j ",
+               i=str(kwargs['i']),
+               j=str(kwargs['j']))
+
+    @staticmethod
+    def _add_edge(tx, **kwargs):
+        """Override"""
+        query = f"MATCH (a:Node),(b:Node) WHERE " \
+            f"a.i = '{int(kwargs['from_i'])}' AND " \
+            f"a.j = '{int(kwargs['from_j'])}' AND " \
+            f"b.i = '{int(kwargs['to_i'])}' AND " \
+            f"b.j = '{int(kwargs['to_j'])}' CREATE (a)-[:CONNECTS]->(b)"
+        # TODO: add weight
+        tx.run(query)
+
+
+if __name__ == '__main__':
+    with open(args.credentials[0]) as f:
+        credentials = json.load(f)
+        graph_generator = GraphGenerator(credentials['neo4j']['uri'],
+                                         credentials['neo4j']['user'],
+                                         credentials['neo4j']['password'], 10,
+                                         10, 0.5)
+        graph_generator.generate_planar_grid_graph()
+        graph_generator.close()
